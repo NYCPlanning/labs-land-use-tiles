fca71190e549a70bb6be34ef4cabb608089fb4fc
Chris Whong <chris@whong.org>
2017-08-09 13:59:17 -0400

    update query

--- scripts/getdata.sh
+++ scripts/getdata.sh
@@ -6,7 +6,7 @@ rm -rf tmp && \
 rm -rf data && \
 [[ -d data ]] || mkdir data && \
 cd tmp && \
-wget --no-check-certificate -O pluto.zip "https://cartoprod.capitalplanning.nyc/user/cpp/api/v2/sql?format=SHP&filename=pluto&q=SELECT the_geom_webmercator as the_geom, landuse, address FROM support_mappluto LIMIT 1" && \
+wget --no-check-certificate -O pluto.zip "https://cartoprod.capitalplanning.nyc/user/cpp/api/v2/sql?format=SHP&filename=pluto&q=SELECT the_geom_webmercator as the_geom, landuse, address FROM support_mappluto" && \
 ls && \
 unzip pluto.zip && \
 rm pluto.zip
