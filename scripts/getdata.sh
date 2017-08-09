d0c349a2bbf6a09d4052f7e46afc9b50cfdb7133
Chris Whong <chris@whong.org>
2017-08-09 15:51:08 -0400

    update query again

--- scripts/getdata.sh
+++ scripts/getdata.sh
@@ -6,7 +6,7 @@ rm -rf tmp && \
 rm -rf data && \
 [[ -d data ]] || mkdir data && \
 cd tmp && \
-wget --no-check-certificate -O pluto.zip "SELECT a.the_geom_webmercator as the_geom, a.landuse, b.description, address FROM support_mappluto a LEFT JOIN support_landuse_lookup b ON a.landuse::integer = b.code" && \
+wget --no-check-certificate -O pluto.zip "https://cartoprod.capitalplanning.nyc/user/cpp/api/v2/sql?format=SHP&filename=pluto&q=SELECT a.the_geom_webmercator as the_geom, a.landuse, b.description, address FROM support_mappluto a LEFT JOIN support_landuse_lookup b ON a.landuse::integer = b.code" && \
 ls && \
 unzip pluto.zip && \
 rm pluto.zip
